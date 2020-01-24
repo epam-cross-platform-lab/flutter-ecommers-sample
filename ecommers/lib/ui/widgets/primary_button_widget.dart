366187650607c30a09318780a778348994eddc75
Anton Marchanka <anton_marchanka@epam.com>
2020-01-24 11:08:12 +0300

    refactor button widgets

--- ecommers/lib/ui/widgets/primary_button_widget.dart
+++ ecommers/lib/ui/widgets/primary_button_widget.dart
@@ -2,24 +2,21 @@ import 'package:ecommers/ui/decorations/index.dart';
 import 'package:ecommers/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
-class PrimaryButtonWidget extends StatelessWidget {
+class PrimaryButtonWidget extends BaseButtonWidget {
   final String text;
   final String assetIcon;
   final Function onPressedFunction;
 
-  PrimaryButtonWidget(
-      {@required this.text,
-      @required this.assetIcon,
-      @required this.onPressedFunction});
-
-  @override
-  Widget build(BuildContext context) {
-    return BaseButtonWidget(
-        text: text,
-        assetIcon: assetIcon,
-        buttonColor: Palette.primaryButtonBackground,
-        textColor: Palette.primaryButtonText,
-        onPressedFunction: onPressedFunction,
-        blurColor: Palette.primaryButtonBlur);
-  }
+  PrimaryButtonWidget({
+    @required this.text,
+    @required this.assetIcon,
+    @required this.onPressedFunction,
+  }) : super(
+          text: text,
+          assetIcon: assetIcon,
+          buttonColor: Palette.primaryButtonBackground,
+          textColor: Palette.primaryButtonText,
+          onPressedFunction: onPressedFunction,
+          blurColor: Palette.primaryButtonBlur,
+        );
 }
--- ecommers/lib/ui/widgets/secondary_button_widget.dart
+++ ecommers/lib/ui/widgets/secondary_button_widget.dart
@@ -2,24 +2,21 @@ import 'package:ecommers/ui/decorations/index.dart';
 import 'package:ecommers/ui/widgets/index.dart';
 import 'package:flutter/material.dart';
 
-class SecondaryButtonWidget extends StatelessWidget {
+class SecondaryButtonWidget extends BaseButtonWidget {
   final String text;
   final String assetIcon;
   final Function onPressedFunction;
 
-  SecondaryButtonWidget(
-      {@required this.text,
-      @required this.assetIcon,
-      @required this.onPressedFunction});
-
-  @override
-  Widget build(BuildContext context) {
-    return BaseButtonWidget(
-        text: text,
-        assetIcon: assetIcon,
-        buttonColor: Palette.secondaryButtonBackground,
-        textColor: Palette.secondaryButtonText,
-        onPressedFunction: onPressedFunction,
-        blurColor: Palette.secondaryButtonBlur);
-  }
+  SecondaryButtonWidget({
+    @required this.text,
+    @required this.assetIcon,
+    @required this.onPressedFunction,
+  }) : super(
+          text: text,
+          assetIcon: assetIcon,
+          buttonColor: Palette.secondaryButtonBackground,
+          textColor: Palette.secondaryButtonText,
+          onPressedFunction: onPressedFunction,
+          blurColor: Palette.secondaryButtonBlur,
+        );
 }
