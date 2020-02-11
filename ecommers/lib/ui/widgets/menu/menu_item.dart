24fa070de29fb45080992f7482862fdfbf464292
Anton Marchanka <anton_marchanka@epam.com>
2020-02-11 12:14:38 +0300

    add all categories page

--- ecommers/lib/ui/widgets/menu/menu_item.dart
+++ ecommers/lib/ui/widgets/menu/menu_item.dart
@@ -3,29 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class MenuItem extends StatelessWidget {
-  static const double _height = 48.0;
-
   final String svgAssetIconPath;
   final String title;
   final String subTitle;
+  final double height;
 
   MenuItem({
     this.svgAssetIconPath,
     this.title,
     this.subTitle,
+    this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: _height,
+      height: height,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(
-            width: 57.0,
-            child: SvgPicture.asset(svgAssetIconPath),
-          ),
+          if (svgAssetIconPath != null)
+            SizedBox(
+              width: 57.0,
+              child: SvgPicture.asset(svgAssetIconPath),
+            )
+          else
+            SizedBox(width: 15.0),
           Expanded(
             child: Text(
               title ?? '',
