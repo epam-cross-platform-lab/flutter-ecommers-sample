751c55252388a6840fec8458d601ea6adb3b73dd
Anton_Marchanka <amag2511@gmail.com>
2020-04-08 13:56:27 +0300

    Revert "Update secured package"
    This reverts commit 2eb39a3046ccb1481994563af7b666c7cf244346.

--- ecommers/lib/core/services/dependency_service.dart
+++ ecommers/lib/core/services/dependency_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
 import '../app_services/index.dart';
@@ -6,6 +7,7 @@ import '../common/index.dart';
 import '../services/extensions/get_it_extension.dart';
 import '../services/index.dart';
 import '../services/navigation/navigation_service.dart';
+
 
 NavigationService get navigationService => GetIt.I.get<NavigationService>();
 FileManager get fileManager => GetIt.I.get<FileManager>();
@@ -31,7 +33,8 @@ class DependencyService {
       ..registerLazySingleton<ProductService>(() => ProductService())
       ..registerLazySingleton<CategoryService>(() => CategoryService())
       ..registerLazySingleton<NoteService>(() => NoteService())
-      ..registerLazySingleton<MembershipService>(() => MembershipService())
+      ..registerLazySingleton<MembershipService>(
+          () => MembershipService(const FlutterSecureStorage()))
       ..registerHttpClient()
       ..registerLazySingleton<CacheDataRepository>(() => CacheDataRepository())
       ..registerSingletonAsync<CacheDatabase>(
--- ecommers/lib/core/services/membership_service.dart
+++ ecommers/lib/core/services/membership_service.dart
@@ -1,13 +1,12 @@
-import 'package:flutter_secured_storage/flutter_secured_storage.dart';
-
 import 'package:ecommers/core/models/login_model.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class MembershipService {
   static const String _accessTokenKey = 'accessTokenKey';
   static const String _refreshTokenKey = 'refreshTokenKey';
   static const String _expirationDateKey = 'expirationDateKey';
 
-  static const FlutterSecuredStorage secureStorage = FlutterSecuredStorage();
+  final FlutterSecureStorage secureStorage;
 
   String _accessToken;
   String _refreshToken;
@@ -17,6 +16,8 @@ class MembershipService {
   String get refreshToken => _refreshToken;
   bool get isNotExpired =>
       _expirationDate != null && DateTime.now().isBefore(_expirationDate);
+
+  MembershipService(this.secureStorage);
 
   Future refresh(LoginModel loginModel) async {
     _accessToken = loginModel.token;
