aeed83998891f149fa7e10d9ea46b767ca67cdff
Yauhen <ysampir@gmail.com>
2020-04-28 13:19:35 +0300

    Add comment

--- ecommers/lib/data/repository/common.mappings.dart
+++ ecommers/lib/data/repository/common.mappings.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 extension AuthExt on AuthResult {
   LoginModel toLoginModel() {
+    //firebase auth do not has tokens
     return LoginModel('token', 'refreshToken',
         DateTime.now().add(const Duration(days: 180)).toString());
   }
